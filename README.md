5cbbc208c0cb69dbc8a34552c6dfadf04359d9a9
SempaiDarcy <mr_darcy_1@mail.ru>
2023-12-05 15:36:17 +0500

    integrated SASS with basic styles, colors, and typography setup

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 ## Technologies:
 - [x] React
 - [x] TypeScript
+- [x] SASS (syntax SCSS)
+---
+### UI Development Tool:
+- [x] Storybook
 ---
 #### Code Quality Tools:
 - [x] ESLint
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import '@/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
