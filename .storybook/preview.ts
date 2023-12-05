6f59961016db24b5a4c7b814402a8e75714e9091
SempaiDarcy <mr_darcy_1@mail.ru>
2023-12-05 15:27:22 +0500

    font installation

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from '@storybook/react'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 const preview: Preview = {
   parameters: {
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { StrictMode } from 'react'
 import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
