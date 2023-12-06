0fed45bc87d81eac2939b54e4b2464d845307e8a
SempaiDarcy <mr_darcy_1@mail.ru>
2023-12-07 03:43:29 +0500

    feat: create new global theme for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,15 +2,28 @@ import type { Preview } from '@storybook/react'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '@/styles/index.scss'
+import { themes } from '@storybook/theming'
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
-    docs: { toc: true },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000',
+        },
+      ],
     },
   },
 }
