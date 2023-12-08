ca6ef472f89ba3f6e08bbda440649f7a2ef34a9c
SempaiDarcy <mr_darcy_1@mail.ru>
2023-12-09 01:06:19 +0500

    feat: add forwardRef support to CheckBox component

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react'
+
 import { Typography } from '@/components/ui/typography'
 import { Indicator, Root } from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
@@ -13,33 +15,35 @@ export type CheckBoxProps = {
   onCheckedChange?: (checked: boolean) => void
 }
 
-export const CheckBox = (props: CheckBoxProps) => {
-  const { checked, className, disabled, label, onCheckedChange } = props
-  const classNames = {
-    buttonCheckBox: clsx(s.buttonCheckBox, disabled && s.disabled),
-    container: clsx(s.container, className),
-    indicator: clsx(s.indicator),
-    labelWrapper: clsx(s.labelWrapper, disabled && s.disabled),
-    root: clsx(s.root),
-  }
+export const CheckBox = forwardRef<HTMLButtonElement, CheckBoxProps>(
+  ({ checked, className, disabled, label, onCheckedChange }, ref) => {
+    const classNames = {
+      buttonCheckBox: clsx(s.buttonCheckBox, disabled && s.disabled),
+      container: clsx(s.container, className),
+      indicator: clsx(s.indicator),
+      labelWrapper: clsx(s.labelWrapper, disabled && s.disabled),
+      root: clsx(s.root),
+    }
 
-  return (
-    <div className={classNames.container}>
-      <Typography as={'label'} className={classNames.labelWrapper} variant={'body2'}>
-        <div className={classNames.buttonCheckBox} tabIndex={0}>
-          <Root
-            checked={checked}
-            className={classNames.root}
-            disabled={disabled}
-            onCheckedChange={onCheckedChange}
-          >
-            <Indicator className={classNames.indicator}>
-              <CheckIcon />
-            </Indicator>
-          </Root>
-        </div>
-        {label}
-      </Typography>
-    </div>
-  )
-}
+    return (
+      <div className={classNames.container}>
+        <Typography as={'label'} className={classNames.labelWrapper} variant={'body2'}>
+          <div className={classNames.buttonCheckBox} tabIndex={0}>
+            <Root
+              checked={checked}
+              className={classNames.root}
+              disabled={disabled}
+              onCheckedChange={onCheckedChange}
+              ref={ref}
+            >
+              <Indicator className={classNames.indicator}>
+                <CheckIcon />
+              </Indicator>
+            </Root>
+          </div>
+          {label}
+        </Typography>
+      </div>
+    )
+  }
+)
