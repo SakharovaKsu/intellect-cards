39b05e440fb933bc9f9014201ef23086a3bbdb58
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-12-09 12:20:37 +0300

    feature: select components

---
+++ src/icons/IconWrapper.tsx
@@ -0,0 +1,36 @@
+import { HTMLProps, ReactNode, SVGProps } from 'react'
+
+export type IconProps = {
+  color?: string
+  size?: number
+  svgProps?: SVGProps<SVGSVGElement>
+} & Omit<HTMLProps<HTMLSpanElement>, 'color' | 'size'>
+
+type IconWrapperProps = { icon: ReactNode } & IconProps
+
+export const IconWrapper = ({
+  color: colorProp,
+  icon,
+  size: sizeProp,
+  ...restProps
+}: IconWrapperProps) => {
+  const color = colorProp ? colorProp : 'currentColor'
+  const size = sizeProp ? `${sizeProp}px` : '24px'
+
+  return (
+    <span
+      aria-hidden
+      role={'img'}
+      style={{
+        color: color,
+        display: 'inline-flex',
+        fontSize: 'inherit',
+        height: size,
+        width: size,
+      }}
+      {...restProps}
+    >
+      {icon}
+    </span>
+  )
+}
